137fd2d2c6ddcc1b6f98bec7f96e83c84e1d6dc2
mikasa <1244456115@qq.com>
2025-05-17 21:34:35 +0800

    爆能枪修改为仅攻击非玩家时无视护甲

--- src/main/java/cn/sh1rocu/esiraoa3extra/mixin/compat/aoa3/DamageUtilMixin.java
+++ src/main/java/cn/sh1rocu/esiraoa3extra/mixin/compat/aoa3/DamageUtilMixin.java
@@ -3,12 +3,14 @@ package cn.sh1rocu.esiraoa3extra.mixin.compat.aoa3;
 import cn.sh1rocu.esiraoa3extra.util.EsirUtil;
 import net.minecraft.core.NonNullList;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.tslat.aoa3.util.DamageUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = DamageUtil.class, remap = false)
@@ -26,5 +28,17 @@ public class DamageUtilMixin {
                 EsirUtil.isPlayerEnvironmentallyProtected(legs) ||
                 EsirUtil.isPlayerEnvironmentallyProtected(feet)
         );
+    }
+
+    @Redirect(
+            method = "dealBlasterDamage",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/damagesource/DamageSource;bypassArmor()Lnet/minecraft/world/damagesource/DamageSource;",
+                    ordinal = 1
+            )
+    )
+    private static DamageSource esir$dealBlasterDamage(DamageSource instance) {
+        return instance;
     }
 }
