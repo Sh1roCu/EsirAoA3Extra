8c135eb5bb9b012f69439c6fcc6b94af058641af
mikasa <1244456115@qq.com>
2025-05-27 21:11:59 +0800

    fix ClientSide

--- src/main/java/cn/sh1rocu/esiraoa3extra/mixin/compat/aoa3/HaulingRodMixin.java
+++ src/main/java/cn/sh1rocu/esiraoa3extra/mixin/compat/aoa3/HaulingRodMixin.java
@@ -23,11 +23,13 @@ public class HaulingRodMixin {
 
     @Inject(method = "use", at = @At("HEAD"), cancellable = true)
     private void esir$fixAoARod(Level level, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-        UUID uuid = player.getUUID();
-        if (!esirAoA3Extra$timeStampMap.containsKey(uuid))
-            esirAoA3Extra$timeStampMap.put(uuid, System.currentTimeMillis());
-        else if (System.currentTimeMillis() - esirAoA3Extra$timeStampMap.get(uuid) < 60)
-            cir.setReturnValue(InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide()));
-        else esirAoA3Extra$timeStampMap.put(uuid, System.currentTimeMillis());
+        if (!level.isClientSide()) {
+            UUID uuid = player.getUUID();
+            if (!esirAoA3Extra$timeStampMap.containsKey(uuid))
+                esirAoA3Extra$timeStampMap.put(uuid, System.currentTimeMillis());
+            else if (System.currentTimeMillis() - esirAoA3Extra$timeStampMap.get(uuid) < 60)
+                cir.setReturnValue(InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide()));
+            else esirAoA3Extra$timeStampMap.put(uuid, System.currentTimeMillis());
+        }
     }
 }
