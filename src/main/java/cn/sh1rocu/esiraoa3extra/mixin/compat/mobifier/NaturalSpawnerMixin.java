69e80ec76efd2a169de04668751f461681cd2449
mikasa <1244456115@qq.com>
2025-06-01 17:19:10 +0800

    修复mobifier修改最大血量后怪物生成时血量不满的问题（可能是混合服务端的原因）

---
+++ src/main/java/cn/sh1rocu/esiraoa3extra/mixin/compat/mobifier/NaturalSpawnerMixin.java
@@ -0,0 +1,28 @@
+package cn.sh1rocu.esiraoa3extra.mixin.compat.mobifier;
+
+import corgitaco.enhancedcelestials.EnhancedCelestialsWorldData;
+import corgitaco.enhancedcelestials.LunarContext;
+import corgitaco.enhancedcelestials.api.lunarevent.LunarMobSpawnInfo;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.NaturalSpawner;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NaturalSpawner.SpawnState.class)
+public class NaturalSpawnerMixin {
+    @Inject(method = "afterSpawn", at = @At("HEAD"))
+    private void esir$afterSpawn(Mob mob, ChunkAccess chunk, CallbackInfo ci) {
+        if (!mob.level.isClientSide()) {
+            LunarContext lunarContext = ((EnhancedCelestialsWorldData) mob.level).getLunarContext();
+            if (lunarContext != null) {
+                LunarMobSpawnInfo lunarSpawner = lunarContext.getCurrentEvent().getLunarSpawner();
+                if (lunarSpawner != null && lunarSpawner.getSpawnInfo().getEntityTypes().contains(mob.getType())) {
+                    mob.setHealth(mob.getMaxHealth());
+                }
+            }
+        }
+    }
+}
